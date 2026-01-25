72ef2fe40715d45e3cf307deb4ee67c3471cde95
Atieh Kashani <atieh.kashani@gmail.com>
2026-01-25 11:41:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -248,7 +248,6 @@ def ingest_event():
 
 
 if __name__ == "__main__":
-    # Local dev only (Render uses gunicorn)
-    port = int(os.environ.get("PORT", "5000"))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
